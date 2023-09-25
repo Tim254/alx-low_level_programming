35d288e42a4a3a07a601d478090b839810ba2b98
Tim254 <timoushawishi@gmail.com>
2023-09-25 11:12:46 -0700

    git push

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void set_string(char **s, char *to);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
