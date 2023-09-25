5e6c1ea3c1cf9af97ec1cf33f4a8fd8f76587b12
Tim254 <timoushawishi@gmail.com>
2023-09-25 11:30:16 -0700

    strstr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ char *_strchr(char *s, char c);
 void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif
