8833482da5dea4eadcbac7524f63e92cad3eef55
Tim254 <timoushawishi@gmail.com>
2023-09-25 11:17:31 -0700

    strprk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 void set_string(char **s, char *to);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
